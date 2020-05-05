3cb6eb25ce401343b188916f1dce1f0c2394f5be
HironoriSato <maza.hs5360@gmail.com>
2020-05-06 00:47:09 +0900

    [add]CartPageを実装

--- src/features/cart/CartPage.jsx
+++ src/features/cart/CartPage.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
-import { addItem } from '../cart/cartSlice'
+import { addItem, removeItem } from '../cart/cartSlice'
 
 import { Container, Row, Col } from 'react-bootstrap'
 
+import CartCards from './CartCards'
+
 const CartPage = () => {
-  const items = useSelector(state => state.cart.items)
+  const total = useSelector(state => state.cart.total)
+  const items = useSelector(state => state.cart.addedItems)
   const dispatch = useDispatch()
 
   return (
     <Container>
-      <Row className="text-center">
+      <Row className="text-center mb-3">
         <Col sm="12">
           <h2>Cart Page</h2>
+        </Col>
+      </Row>
+      <Row className="text-center mb-3">
+        <Col sm="12">
+          <h3>Total Prce: ${total}</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <CartCards
+            items={items}
+            removeItem={id => { dispatch(removeItem(id)) }}
+            addItem={(id, cnt) => { dispatch(addItem({id, cnt})) }} />
         </Col>
       </Row>
     </Container>
