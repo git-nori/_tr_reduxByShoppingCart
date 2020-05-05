c4364ca9e26341112193641f42ae52360fdecaa5
HironoriSato <maza.hs5360@gmail.com>
2020-05-05 22:38:17 +0900

    [add]各ページのルーティングを設定

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import CommonHeader from '../components/CommonHeader'
 import ItemListPage from '../features/itemList/ItemListPage'
+import CartPage from '../features/cart/CartPage'
 
 function App () {
   return (
     <div className="App">
       <CommonHeader />
-      <ItemListPage />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ItemListPage} />
+          <Route exact path="/cart" component={CartPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
