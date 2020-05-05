a58b0bde468e872f31540514512b6b3a0ec16b67
HironoriSato <maza.hs5360@gmail.com>
2020-05-06 00:47:50 +0900

    [add]cart/CartForms orgを実装

---
+++ src/features/cart/CartForms.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Button, Form } from 'react-bootstrap'
+import { BsPlus, BsDash } from "react-icons/bs";
+
+const CartForms = ({ item, addItem, removeItem }) => {
+
+  const handleSubClick = () => {
+    if(item.quantity > 0){
+      addItem(item.id, -1)
+    }
+  }
+
+  return (
+    <Form inline>
+      <Form.Group>
+        <Form.Control value={item.quantity} onChange={()=>{addItem(item.id, null)}}></Form.Control>
+      </Form.Group>
+      <div className="mt-3">
+        <Button variant="outline-info" onClick={()=>{addItem(item.id, null)}}><BsPlus /></Button>
+        <Button className="ml-1" variant="outline-info" onClick={handleSubClick}><BsDash /></Button>
+        <Button className="ml-2" variant="outline-danger" onClick={() => { removeItem(item.id) }}>Remove</Button>
+      </div>
+    </Form>
+  )
+}
+
+export default CartForms
